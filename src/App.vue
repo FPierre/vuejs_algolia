1dd8f464874dacb1bcb4749dbe262f65735d4287
FPierre <pflauder@gmail.com>
2016-07-15 22:28:25 +0200

    Installation de autocomplete.js

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,16 @@
   <div id="app">
     <h1>{{ msg }}</h1>
 
-    <input id="search-box" type="text">
+    <input id="search-input" type="text">
 
     <div id="hits-container"></div>
   </div>
 </template>
 
 <script>
-// var algoliasearch = require('algoliasearch')
+let algoliasearch = require('algoliasearch')
 // let instantsearch = require('instantsearch.js')
+let autocomplete = require('../vendor/autocomplete.min.js')
 
 export default {
   data () {
@@ -19,16 +20,16 @@ export default {
     }
   },
   ready: () => {
-    /*
     var client = algoliasearch('6DLEN782G1', '6ca42b1a471f2cc5def1aa1fedd10257')
     var index = client.initIndex('dev_knowledges')
 
+    /*
     index.search('git', function searchDone(err, content) {
       console.log(err, content);
     })
     */
 
-    /* 
+    /*
     let search = instantsearch({
       appId: '6DLEN782G1',
       apiKey: '6ca42b1a471f2cc5def1aa1fedd10257',
@@ -42,7 +43,7 @@ export default {
         placeholder: 'Search for knowledges'
       })
     )
-    
+
     // Add a hits widget
     search.addWidget(
       instantsearch.widgets.hits({
@@ -53,12 +54,55 @@ export default {
 
     search.start()
     */
+
+    autocomplete('#search-input', { hint: false }, [
+      {
+        source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+      	displayKey: 'tags',
+        templates: {
+          suggestion: function (suggestion) {
+            return suggestion._highlightResult.tags.value
+          }
+        }
+      }
+    ]).on('autocomplete:selected', function (event, suggestion, dataset) {
+      console.log(suggestion, dataset)
+    })
   }
 }
 </script>
 
 <style>
-body {
-  font-family: Helvetica, sans-serif;
+.algolia-autocomplete {
+  width: 100%;
+}
+
+.algolia-autocomplete .aa-input, .algolia-autocomplete .aa-hint {
+  width: 100%;
+}
+
+.algolia-autocomplete .aa-hint {
+  color: #999;
+}
+
+.algolia-autocomplete .aa-dropdown-menu {
+  width: 100%;
+  background-color: #fff;
+  border: 1px solid #999;
+  border-top: none;
+}
+
+.algolia-autocomplete .aa-dropdown-menu .aa-suggestion {
+  cursor: pointer;
+  padding: 5px 4px;
+}
+
+.algolia-autocomplete .aa-dropdown-menu .aa-suggestion.aa-cursor {
+  background-color: #B2D7FF;
+}
+
+.algolia-autocomplete .aa-dropdown-menu .aa-suggestion em {
+  font-weight: bold;
+  font-style: normal;
 }
 </style>

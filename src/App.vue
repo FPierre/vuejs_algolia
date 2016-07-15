70dc752a05eaded1b64cef62168918a5fd0a6bd8
FPierre <pflauder@gmail.com>
2016-07-15 22:31:17 +0200

    Version autocomplete.js

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,22 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
-
     <input id="search-input" type="text">
-
-    <div id="hits-container"></div>
   </div>
 </template>
 
 <script>
 let algoliasearch = require('algoliasearch')
-// let instantsearch = require('instantsearch.js')
 let autocomplete = require('../vendor/autocomplete.min.js')
 
 export default {
   data () {
     return {
-      msg: 'Hello Vue!'
+
     }
   },
   ready: () => {
     var client = algoliasearch('6DLEN782G1', '6ca42b1a471f2cc5def1aa1fedd10257')
     var index = client.initIndex('dev_knowledges')
-
-    /*
-    index.search('git', function searchDone(err, content) {
-      console.log(err, content);
-    })
-    */
-
-    /*
-    let search = instantsearch({
-      appId: '6DLEN782G1',
-      apiKey: '6ca42b1a471f2cc5def1aa1fedd10257',
-      indexName: 'dev_knowledges'
-    })
-
-    // Add a searchBox widget
-    search.addWidget(
-      instantsearch.widgets.searchBox({
-        container: '#search-box',
-        placeholder: 'Search for knowledges'
-      })
-    )
-
-    // Add a hits widget
-    search.addWidget(
-      instantsearch.widgets.hits({
-        container: '#hits-container',
-        hitsPerPage: 10
-      })
-    )
-
-    search.start()
-    */
 
     autocomplete('#search-input', { hint: false }, [
       {
